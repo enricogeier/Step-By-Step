aa2bb7436c156cb1716cb1d452459fd270a26131
enricogeier <90223595+enricogeier@users.noreply.github.com>
2023-11-13 16:25:11 +0100

    button 1

---
+++ Destroy Everything!/Assets/Scripts/Button.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private float button_start_y;
+    [SerializeField] private float button_pressed_y;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+}
