c2afbda9f9079d783d22da0e36a8c9d5bce59781
enricogeier <90223595+enricogeier@users.noreply.github.com>
2023-12-02 21:29:18 +0100

    moving platform

--- Destroy Everything!/Assets/Scripts/Platform.cs
+++ Destroy Everything!/Assets/Scripts/Platform.cs
@@ -6,32 +6,126 @@ public class Platform : MonoBehaviour
 {
 
     private Vector3 startPosition;
+    private bool playerOnPlatform = false;
 
-    [SerializeField] private float rotateXVelocity = 0.0f;
-    [SerializeField] private float rotateYVelocity = 0.0f;
-    [SerializeField] private float rotateZVelocity = 0.0f;
+    private Vector3 lastRotation = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 deltaRotation;
 
-    [SerializeField] private float moveXVelocity = 0.0f;
-    [SerializeField] private float moveYVelocity = 0.0f;
-    [SerializeField] private float moveZVelocity = 0.0f;
+    private Vector3 lastPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 deltaPosition;
 
+    private bool wait = false;
+
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private bool rotateX = false;
+    [SerializeField] private bool rotateY = false;
+    [SerializeField] private bool rotateZ = false;
+
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float rotationDistance = 0.0f; // default: 10.0f
     [SerializeField] private float moveDistance = 0.0f;
+    [SerializeField] private float wait_time;
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            playerOnPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            playerOnPlatform = false;
+        }
+    }
+
+
+    private void afterWait()
+    {
+        wait = false;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;        
+        startPosition = transform.position;
+        if(rotateX && (rotateY || rotateZ) || rotateY && rotateZ)
+        {
+            rotateX = false;
+            rotateY = false;
+            rotateZ = false;
+
+            Debug.LogError("Platform can not rotate on two axis!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(rotateXVelocity, rotateYVelocity, rotateZVelocity);
+       
+        if(rotateX)
+        {
+            gameObject.transform.Rotate(rotationDistance * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else if(rotateY)
+        {
+            gameObject.transform.Rotate(0.0f, rotationDistance * Time.deltaTime, 0.0f);
+        }
+        else if(rotateZ)
+        {
+            gameObject.transform.Rotate(0.0f, 0.0f, rotationDistance * Time.deltaTime);
+        }
         
-        // TODO: implement object movement
+        deltaRotation = transform.rotation.eulerAngles - lastRotation;
 
+        if(!wait)
+        {
+
+            gameObject.transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            deltaPosition = transform.position - lastPosition;
+
+            if (Vector3.Distance(gameObject.transform.position, startPosition) > moveDistance)
+            {
+                wait = true;
+                Invoke("afterWait", wait_time);
+
+                moveDirection *= -1;
+                startPosition = gameObject.transform.position;
+                
+            }
+
+        }
+
+        if (!wait)
+        {
+
+            // Player 
+            if (playerOnPlatform && player != null)
+            {
+
+                player.GetComponent<CharacterController>().transform.position += new Vector3(deltaPosition.x, 0.0f, deltaPosition.y);
+                player.GetComponent<CharacterController>().Move(moveDirection * moveSpeed * Time.deltaTime);
+
+
+
+            }
+
+            lastPosition = transform.position;
+
+        }
+
+        lastRotation = transform.rotation.eulerAngles;
+        
+
+        
 
     }
 }
